513afb4773ee401ec34298edd30129fa38ac85a5
benmelz <ben@melz.me>
2025-06-25 11:24:54 -0400

    set up vitest

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,22 @@ import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      provider: 'playwright',
+      headless: true,
+      instances: [
+        { browser: 'chromium' }
+      ]
+    },
+    coverage: {
+      enabled: true,
+      include: ['src/**/*'],
+      exclude: ['src/index.jsx'],
+      thresholds: {
+        100: true,
+      },
+    },
+  },
 })
