e4b5c313804d55cf5ef5176b6c7145790e41bcf8
benmelz <ben@melz.me>
2025-06-26 16:49:47 -0400

    add a clock for fun

--- src/components/Stop.jsx
+++ src/components/Stop.jsx
@@ -1,9 +1,19 @@
+import { format } from 'date-fns'
+import { useEffect, useState } from 'react'
 import classNames from './Stop.module.css'
 
 export default function Stop ({ name, children }) {
+  const [time, setTime] = useState(new Date())
+  useEffect(() => {
+    const interval = setInterval(() => setTime(new Date()), 1000)
+    return () => clearInterval(interval)
+  }, [])
   return (
     <div className={classNames['stop-container']}>
-      <div className={classNames['stop-name']}>{name}</div>
+      <div className={classNames['stop-header']}>
+        <div>{name}</div>
+        <div>{format(time, 'h:mm aaa')}</div>
+      </div>
       <hr />
       <div className={classNames['stop-departures']}>{children}</div>
     </div>
