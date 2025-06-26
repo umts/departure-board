9e429c97a51b5adc3d1769de58681bd3295de3e5
benmelz <ben@melz.me>
2025-06-26 16:25:36 -0400

    time switching

--- src/components/Departure.jsx
+++ src/components/Departure.jsx
@@ -1,8 +1,14 @@
-import { formatDistanceToNow } from 'date-fns'
+import { format, formatDistanceToNow } from 'date-fns'
+import { useEffect, useState } from 'react'
 import classNames from './Departure.module.css'
 
 export default function Departure ({ route, destination, time, color, textColor }) {
-  const relativeTime = formatDistanceToNow(time)
+  const [timeToggle, setTimeToggle] = useState(true)
+  useEffect(() => {
+    const timeout = setTimeout(() => setTimeToggle(!(timeToggle)), 5000)
+    return () => clearTimeout(timeout)
+  }, [timeToggle])
+
   return (
     <div className={classNames['departure-container']} style={{ backgroundColor: color, color: textColor }}>
       <div className={classNames['departure-route']}>
@@ -12,7 +18,7 @@ export default function Departure ({ route, destination, time, color, textColor 
         {destination}
       </div>
       <div className={classNames['departure-time']}>
-        {relativeTime}
+        {(timeToggle) ? format(time, 'h:mm aaa') : formatDistanceToNow(time)}
       </div>
     </div>
   )
