aba8b81dd8981a7c478d8ba234247fd6971016fd
Louis Pate <louispate@gmail.com>
2025-06-26 15:18:16 -0400

    Set up my hypothetical function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import DepartureBoard from './components/DepartureBoard.jsx'
+import { useGtfsSchedule, useGtfsRealtime, useFetchResolver } from 'gtfs-react-hooks'
+
+const scheduleResolver = useFetchResolver("http://localhost:9292/gtfs")
+const gtfsSchedule = useGtfsSchedule(scheduleResolver, 24 * 60 * 60 * 1000)
+
+const tripUpdatesResolver = useFetchResolver("http://localhost:9292/gtfs-rt/trip-updates")
+const gtfsTripUpdates = useGtfsRealtime(tripUpdatesResolver, 30 * 1000)
+
+const stopId = 64
+const departures = departuresFromGtfs(gtfsSchedule, gtfsTripUpdates, stopId)
 
 export default function App () {
   return (<DepartureBoard />)
