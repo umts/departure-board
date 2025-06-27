2c85eb94a0df136d3df162a45c0d76e94c57bd23
benmelz <ben@melz.me>
2025-06-27 12:33:30 -0400

    no more timer, we can add it back later as a niceity if needed

--- src/components/Stop.jsx
+++ src/components/Stop.jsx
@@ -1,18 +1,10 @@
-import { format } from 'date-fns'
-import { useEffect, useState } from 'react'
 import classNames from './Stop.module.css'
 
 export default function Stop ({ name, children }) {
-  const [time, setTime] = useState(new Date())
-  useEffect(() => {
-    const interval = setInterval(() => setTime(new Date()), 1000)
-    return () => clearInterval(interval)
-  }, [])
   return (
     <div className={classNames['stop-container']}>
       <div className={classNames['stop-header']}>
-        <div>{name}</div>
-        <div>{format(time, 'h:mm aaa')}</div>
+        {name}
       </div>
       <hr />
       <div className={classNames['stop-departures']}>{children}</div>
