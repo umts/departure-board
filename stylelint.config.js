95eb2d44f4049b4bfce722dcf156dde4327f8c77
benmelz <ben@melz.me>
2025-06-30 13:57:51 -0400

    add sass support

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,5 +1,9 @@
 export default {
   extends: 'stylelint-config-standard',
+  overrides: [{
+    files: ['**/*.scss'],
+    extends: 'stylelint-config-standard-scss'
+  }],
   ignoreFiles: [
     'coverage/**/*',
     'dist/**/*',
