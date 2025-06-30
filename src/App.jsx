53ed86c2437fc9c1f41de20e550bc230b48c3327
Louis Pate <louispate@gmail.com>
2025-06-30 14:15:32 -0400

    Allow many stop ids to be passed to `departuresFromGtfs` (#10)
    * Map over array of stop ids
    
    * Semicolon
    
    * Key stop on stop id, enforce it's uniqueness
    
    * Don't encode craziness into the app yet
    
    * Encoded wrong stop id

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,9 @@ export default function App () {
   const tripUpdatesResolver = useFetchResolver('http://localhost:9292/gtfs-rt/trip-updates')
   const gtfsTripUpdates = useGtfsRealtime(tripUpdatesResolver, 30 * 1000)
 
-  const stopId = '116'
-  const departures = departuresFromGtfs(gtfsSchedule, gtfsTripUpdates, stopId)
+  const stopIds = ['116']
+  const maybeDepartures = departuresFromGtfs(gtfsSchedule, gtfsTripUpdates, stopIds)
+  const departures = maybeDepartures !== undefined ? maybeDepartures[0] : undefined
 
   return (
     <>
