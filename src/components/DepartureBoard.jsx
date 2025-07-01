75f03d3f5de238a11cdf2867d440ecffdb50b380
benmelz <ben@melz.me>
2025-07-01 14:00:18 -0400

    temp

--- src/components/DepartureBoard.jsx
+++ src/components/DepartureBoard.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import { format, formatDistanceToNow } from 'date-fns'
 import TimeFormatterContext from '../contexts/DepartureTimeFormatterContext.js'
-import classNames from './DepartureBoard.module.css'
+import classNames from './DepartureBoard.module.scss'
 
 export default function DepartureBoard ({ children }) {
   const [timeFormatter, setTimeFormatter] = useState(() => absoluteTime)
@@ -12,8 +12,26 @@ export default function DepartureBoard ({ children }) {
     return () => clearTimeout(timeout)
   }, [timeFormatter])
 
+  const [aspectRatio, setAspectRatio] = useState(getAspectRatio())
+
+  useEffect(() => {
+    const handleResize = () => {
+      setAspectRatio(getAspectRatio())
+    }
+    window.addEventListener('resize', handleResize)
+    return () => window.removeEventListener('resize', handleResize)
+  }, [])
+
+  const multi = Math.floor(Math.sqrt(children.length / (aspectRatio[0] * aspectRatio[1])))
+
+  const gridWidth = aspectRatio[0] * multi
+  const gridHeight = aspectRatio[1] * multi
+
+  console.log(`aspectRatio: ${aspectRatio}`)
+  console.log(`dim: ${gridWidth} ${gridHeight}`)
+
   return (
-    <div className={classNames['departure-board']}>
+    <div className={classNames['departure-board']} style={{ gridTemplateColumns: `repeat(${1}, 1fr)` }}>
       <TimeFormatterContext value={timeFormatter}>
         {children}
       </TimeFormatterContext>
@@ -24,3 +42,14 @@ export default function DepartureBoard ({ children }) {
 const absoluteTime = (time) => format(time, 'h:mm aaa')
 
 const relativeTime = formatDistanceToNow
+
+function getAspectRatio () {
+  const width = window.innerWidth
+  const height = window.innerHeight
+
+  const min = Math.min(width, height)
+  const x = width / min
+  const y = height / min
+
+  return [Math.round(x), Math.round(y)]
+}
