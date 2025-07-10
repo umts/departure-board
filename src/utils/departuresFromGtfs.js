48250d6195b82660ec095be34f694b1a8b18cfe0
Louis Pate <louispate@gmail.com>
2025-07-10 10:25:57 -0400

    Refactor departure time parsing (#18)
    * Breakout main parsing code
    
    * Just map times directly
    
    * Sane function name
    
    * Need to pass schedule, updates to function
    
    * Don't spread stop ids until after we've mapped to departures
    
    * Way trim down route id searching
    
    * lint
    
    * Enforce uniqueness on route ids
    
    * Optimize shape filtering
    
    * Ensure we're showing the soonest future departure
    
    * Redo mapping of updates to our format
    
    * Clean up uniqueness enforcement on stop ids
    
    * Keep skipped stop const in its block
    
    * Unify/format departures property processing
    
    * Unify the entire departure transform into one chain
    
    * Correctly drop undefined stops

--- src/utils/departuresFromGtfs.js
+++ src/utils/departuresFromGtfs.js
@@ -1,8 +1,6 @@
-import { isBefore, fromUnixTime } from 'date-fns'
+import { isFuture, fromUnixTime } from 'date-fns'
 /* v8 ignore next */
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
-
-const STOP_SKIPPED = GtfsRealtimeBindings.transit_realtime.TripUpdate.StopTimeUpdate.ScheduleRelationship.SKIPPED
 
 export default function departuresFromGtfs (gtfsSchedule, gtfsTripUpdates, stopIds) {
   if (gtfsSchedule?.stops === undefined ||
@@ -13,67 +11,52 @@ export default function departuresFromGtfs (gtfsSchedule, gtfsTripUpdates, stopI
   }
 
   stopIds = [...new Set(stopIds)]
+  return stopIds
+    .map((stopId) => getStopDepartures(gtfsSchedule, gtfsTripUpdates, stopId))
+    .filter((departures) => !!departures)
+}
 
-  return stopIds.map((stopId) => {
-    const stop = gtfsSchedule.stops.find((stop) => stop.stopId === stopId)
-    /* v8 ignore next */
-    if (stop === undefined) return undefined
+function getStopDepartures (gtfsSchedule, gtfsTripUpdates, stopId) {
+  const stop = gtfsSchedule.stops.find((stop) => stop.stopId === stopId)
+  /* v8 ignore next */
+  if (stop === undefined) return undefined
 
-    const result = { stopId: stop.stopId, stopName: stop.stopName }
-
-    // Isolate relevant trips based on our stop id
-    const tripIds = gtfsSchedule.stopTimes
-      .filter((stopTime) => stopTime.stopId === stopId)
-      .map((stopTime) => stopTime.tripId)
-
-    // Create then splat a Set to enforce uniqueness after mapping trip ids to their routes
-    const routeIds = [...new Set(
-      gtfsSchedule.trips.filter((trip) => tripIds.includes(trip.tripId)).map((trip) => trip.routeId)
-    )]
-
-    // Now retain updates which are for stops on our routes at this stop which haven't been cancelled
-    const relevantUpdates = gtfsTripUpdates.entity
-      .map((entity) => entity.tripUpdate)
-      .filter((tripUpdate) => routeIds.includes(tripUpdate.trip.routeId)) // Only want updates relevant to our routes
-      .filter((tripUpdate) => tripUpdate.stopTimeUpdate.some((stopTimeUpdate) =>
-        stopTimeUpdate.stopId === stopId &&                       // Only want updates relevant to our stop
-        stopTimeUpdate.scheduleRelationship !== STOP_SKIPPED &&   // We just want the next departure, ignore skipped stops
-        tripUpdate.stopTimeUpdate[tripUpdate.stopTimeUpdate.length - 1] !== stopTimeUpdate // If it's the last stop in the sequence, we don't care about it's arrival
-      ))
-
-    // GTFS deals with many destinations on a route though shapes. We're really showing the time till the next bus
-    // servicing each shape at this stop. So go through our updates and find the earliest departure on each shape.
-    const shapeToNextDepartureMap = new Map()
-    relevantUpdates.forEach((tripUpdate) => {
-      const trip = gtfsSchedule.trips.find((trip) => trip.tripId === tripUpdate.trip.tripId)
-      const shapeId = trip.shapeId
-      const departureTime = fromUnixTime(
+  const STOP_SKIPPED = GtfsRealtimeBindings.transit_realtime.TripUpdate.StopTimeUpdate.ScheduleRelationship.SKIPPED
+  const processedShapes = new Set()
+  const result = { stopId: stop.stopId, stopName: stop.stopName }
+  result.departures = gtfsTripUpdates.entity
+    .map((entity) => entity.tripUpdate)
+    .map((tripUpdate) => {
+      const stopTimeUpdate = tripUpdate.stopTimeUpdate.find((stopTimeUpdate) => stopTimeUpdate.stopId === stopId)
+      if (
+        stopTimeUpdate &&
+        stopTimeUpdate.scheduleRelationship !== STOP_SKIPPED &&
+        tripUpdate.stopTimeUpdate[tripUpdate.stopTimeUpdate.length - 1] !== stopTimeUpdate
+      ) {
         /* v8 ignore next */
-        tripUpdate.stopTimeUpdate.find((stopTimeUpdate) => stopTimeUpdate.stopId === stopId)?.departure?.time ||
-        /* v8 ignore next */
-        tripUpdate.stopTimeUpdate.find((stopTimeUpdate) => stopTimeUpdate.stopId === stopId).arrival.time
-      )
-      if (shapeToNextDepartureMap.get(shapeId) === undefined ||
-          isBefore(departureTime, shapeToNextDepartureMap.get(shapeId).departureTime)) {
-        shapeToNextDepartureMap.set(shapeId, { departureTime, trip })
+        const departureTime = fromUnixTime((stopTimeUpdate.departure || stopTimeUpdate.arrival).time)
+        const trip = gtfsSchedule.trips.find((trip) => trip.tripId === tripUpdate.trip.tripId)
+        const shapeId = trip.shapeId
+        if (!processedShapes.has(shapeId) && isFuture(departureTime)) {
+          processedShapes.add(shapeId)
+          return { departureTime, trip }
+        }
       }
+      return undefined
     })
-
-    // Now map over those shapes to our departures format
-    result.departures = []
-    shapeToNextDepartureMap.values().forEach((departure) => {
+    .filter((departure) => !!departure)
+    .map((departure) => {
       const route = gtfsSchedule.routes.find((route) => route.routeId === departure.trip.routeId)
-      result.departures.push({
+      return {
         id: departure.trip.tripId,
         destination: departure.trip.tripHeadsign,
         route: route.routeShortName,
         time: departure.departureTime,
         color: `#${route.routeColor}`,
         sortOrder: route.routeSortOrder,
-      })
+      }
     })
+    .sort((departure1, departure2) => Number(departure1.sortOrder) - Number(departure2.sortOrder))
 
-    result.departures.sort((departure1, departure2) => Number(departure1.sortOrder) - Number(departure2.sortOrder))
-    return result
-  })
+  return result
 }
