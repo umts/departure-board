27937a75953349a8db02ed7553241b5fe686db03
benmelz <ben@melz.me>
2025-08-11 13:49:22 -0400

    switch to non-fixture mocked data

--- test/App.test.jsx
+++ test/App.test.jsx
@@ -1,8 +1,9 @@
-import { describe, expect, it, vi } from 'vitest'
+import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { render } from 'vitest-browser-react'
 import App from '../src/App.jsx'
-import gtfsSchedule from './fixtures/schedule.json'
-import gtfsTripUpdates from './fixtures/trip-updates.json'
+
+const ScheduleRelationship = GtfsRealtimeBindings.transit_realtime.TripUpdate.StopTimeUpdate.ScheduleRelationship
 
 const gtfsReactHooksMocks = vi.hoisted(() => ({
   useGtfsSchedule: vi.fn(),
@@ -16,41 +17,56 @@ vi.mock('gtfs-react-hooks', () => ({
   useFetchResolver: gtfsReactHooksMocks.useFetchResolver,
 }))
 
+function configureApp ({ stopIds }) {
+  const url = new URL(location)
+  url.searchParams.set('stopIds', stopIds)
+  history.pushState({}, '', url)
+}
+
 describe('App', () => {
+  const currentUnixTime = 43200 // 12 pm
+
+  beforeEach(() => {
+    vi.setSystemTime(new Date(currentUnixTime * 1000))
+  })
+
   it('renders nothing when no data has been fetched', async () => {
     gtfsReactHooksMocks.useGtfsSchedule.mockImplementation(() => undefined)
     gtfsReactHooksMocks.useGtfsRealtime.mockImplementation(() => undefined)
-    const url = new URL(location)
-    url.searchParams.set('stopIds', '116')
-    history.pushState({}, '', url)
 
+    configureApp({ stopIds: 'MY_STOP' })
     const { container } = render(<App />)
+
     await expect(container).toBeEmptyDOMElement()
   })
 
   it('renders departures when data has been fetched', async () => {
-    const currentTime = new Date(2025, 5, 27, 16, 20)
-    vi.setSystemTime(currentTime)
+    gtfsReactHooksMocks.useGtfsSchedule.mockImplementation(() => ({
+      routes: [{ routeId: 'MY_ROUTE', routeShortName: 'MR', routeColor: '111111' }],
+      trips: [{ tripId: 'MY_TRIP', routeId: 'MY_ROUTE', tripHeadsign: 'My trip' }],
+      stops: [{ stopId: 'MY_STOP', stopName: 'My stop' }],
+    }))
+    gtfsReactHooksMocks.useGtfsRealtime.mockImplementation(() => ({
+      entity: [
+        {
+          tripUpdate: {
+            trip: { tripId: 'MY_TRIP' },
+            stopTimeUpdate: [
+              {
+                stopId: 'MY_STOP',
+                scheduleRelationship: ScheduleRelationship.SCHEDULED,
+                departure: { time: currentUnixTime + (60 * 5) }
+              }
+            ]
+          }
+        },
+      ]
+    }))
 
-    gtfsReactHooksMocks.useGtfsSchedule.mockImplementation(() => gtfsSchedule)
-    gtfsReactHooksMocks.useGtfsRealtime.mockImplementation(() => gtfsTripUpdates)
-    const url = new URL(location)
-    url.searchParams.set('stopIds', '116')
-    history.pushState({}, '', url)
+    configureApp({ stopIds: 'MY_STOP' })
+    const { getByRole } = render(<App />)
 
-    const { getByText } = render(<App />)
-    await expect.element(getByText('Amherst College')).toBeVisible()
-    await expect.element(getByText('B43')).toBeVisible()
-    await expect.element(getByText('Northampton via Hampshire Mall')).toBeVisible()
-    await expect.element(getByText('4:23 pm')).toBeVisible()
-
-    vi.advanceTimersByTime(5000)
-    await expect.element(getByText('3 minutes')).toBeVisible()
-
-    vi.advanceTimersByTime(5000)
-    await expect.element(getByText('4:23 pm')).toBeVisible()
-
-    // TODO: Actually test.
-    window.dispatchEvent(new Event('resize'))
+    const departure = getByRole('listitem').filter({ hasText: 'MRMy trip12:05 pm' })
+    await expect(departure).toBeVisible()
   })
 })
--- test/globalSetup.js
+++ test/globalSetup.js
@@ -1,3 +1,3 @@
 export const setup = () => {
-  process.env.TZ = 'US/Eastern'
+  process.env.TZ = 'Etc/GMT'
 }
