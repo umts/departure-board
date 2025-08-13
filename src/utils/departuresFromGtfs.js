ea0b3165ba54313649da2991ed63ea9b70411845
benmelz <ben@melz.me>
2025-08-13 11:34:00 -0400

    reimplementation

--- src/utils/departuresFromGtfs.js
+++ src/utils/departuresFromGtfs.js
@@ -1,6 +1,8 @@
 import { fromUnixTime, isPast } from 'date-fns'
 import GtfsRealtimeBindings from 'gtfs-realtime-bindings'
 
+const ScheduleRelationship = GtfsRealtimeBindings.transit_realtime.TripUpdate.StopTimeUpdate.ScheduleRelationship
+
 export default function departuresFromGtfs (gtfsSchedule, gtfsTripUpdates, stopIds) {
   if (gtfsSchedule?.routes === undefined ||
       gtfsSchedule?.stops === undefined ||
@@ -9,56 +11,76 @@ export default function departuresFromGtfs (gtfsSchedule, gtfsTripUpdates, stopI
     return undefined
   }
 
+  console.log('new departures')
+
   const routesById = buildIndex(gtfsSchedule.routes, (route) => route.routeId)
   const stopsById = buildIndex(gtfsSchedule.stops, (stop) => stop.stopId)
   const tripsById = buildIndex(gtfsSchedule.trips, (trip) => trip.tripId)
-  stopIds = new Set(stopIds)
 
-  const uniqueDepartures = {}
+  const stops = [...new Set(stopIds)].map((stopId) => stopsById[stopId]).filter(Boolean)
+  const updates = earliestStopTimeUpdates(gtfsTripUpdates.entity.map((entity) => entity.tripUpdate), tripsById)
 
-  gtfsTripUpdates.entity.forEach((entity) => {
-    const trip = tripsById[entity.tripUpdate.trip.tripId]
-    if (trip === undefined) return
+  const departures = []
+  stops.forEach((stop) => {
+    departures.push({
+      id: stop.stopId,
+      name: stop.stopName,
+      departures: updates.filter((update) => update.stopId === stop.stopId).map((update) => {
+        const trip = tripsById[update.tripId]
+        const route = routesById[update.routeId]
+        if (trip === undefined || route === undefined) return undefined
 
-    entity.tripUpdate.stopTimeUpdate.forEach((stopTimeUpdate) => {
-      if (stopTimeUpdate.scheduleRelationship !== ScheduleRelationship.SCHEDULED) return
-
-      const route = routesById[trip.routeId]
-      if (route === undefined) return
-
-      const stop = stopsById[stopTimeUpdate.stopId]
-      if (stop === undefined) return
-      if (!(stopIds.has(stop.stopId))) return
-
-      const time = fromUnixTime((stopTimeUpdate.departure || stopTimeUpdate.arrival).time)
-      if (isPast(time)) return
-
-      uniqueDepartures[stop.stopId] ??= {}
-      uniqueDepartures[stop.stopId][route.routeId] ??= {}
-      const previousDeparture = uniqueDepartures[stop.stopId][route.routeId][trip.shapeId]
-      if (previousDeparture === undefined || previousDeparture.time > time) {
-        uniqueDepartures[stop.stopId][route.routeId][trip.shapeId] = { tripId: trip.tripId, time }
-      }
+        return {
+          id: `${update.shapeId}-${update.routeId}`,
+          destination: trip.tripHeadsign,
+          route: route.routeShortName,
+          time: update.time,
+          color: `#${route.routeColor}`
+        }
+      }).filter(Boolean)
     })
   })
-
-  return [...stopIds].map((stopId) => stopsById[stopId]).filter(Boolean).map((stop) => ({
-    id: stop.stopId,
-    name: stop.stopName,
-    departures: Object.values(uniqueDepartures[stop.stopId] || {})
-      .flatMap((byShape) => Object.values(byShape))
-      .map(({ tripId, time }) => ({
-        id: tripId,
-        destination: tripsById[tripId].tripHeadsign,
-        route: routesById[tripsById[tripId].routeId].routeShortName,
-        time,
-        color: `#${routesById[tripsById[tripId].routeId].routeColor}`
-      }))
-  }))
+  return departures
 }
 
 function buildIndex (collection, computeKey) {
   const index = {}
   collection.forEach((item) => { index[computeKey(item)] ??= item })
   return index
+}
+
+function earliestStopTimeUpdates (tripUpdates, tripsById) {
+  const stopTimeUpdates = {}
+  tripUpdates.forEach((tripUpdate) => {
+    const trip = tripsById[tripUpdate.trip.tripId]
+    if (trip === undefined) return
+
+    tripUpdate.stopTimeUpdate.forEach((stopTimeUpdate) => {
+      if (stopTimeUpdate.scheduleRelationship !== ScheduleRelationship.SCHEDULED) return
+
+      const shapeId = trip.shapeId
+      const stopId = stopTimeUpdate.stopId
+      const routeId = trip.routeId
+      const tripId = trip.tripId
+      const time = fromUnixTime((stopTimeUpdate.departure || stopTimeUpdate.arrival).time)
+
+      if (isPast(time)) return
+
+      stopTimeUpdates[stopId] ??= {}
+      stopTimeUpdates[stopId][routeId] ??= {}
+      const previousDeparture = stopTimeUpdates[stopId][routeId][shapeId]
+      if (previousDeparture === undefined || previousDeparture.time > time) {
+        stopTimeUpdates[stopId][routeId][shapeId] = { shapeId, stopId, tripId, routeId, time }
+      }
+    })
+  })
+  const departures = []
+  Object.values(stopTimeUpdates).forEach((byRoute) => {
+    Object.values(byRoute).forEach((byShape) => {
+      Object.values(byShape).forEach((departure) => {
+        departures.push(departure)
+      })
+    })
+  })
+  return departures
 }
