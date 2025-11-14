461193987560db47a510be20bc4ccc5d3a6bb3ea
benmelz <ben@melz.me>
2025-11-14 10:24:48 -0500

    Revert "Filter out stop time updates which are the last stop in a trip"
    This reverts commit 2144583bdb908e7531a660748117dbb394ec98f7.

--- src/utils/departuresFromGtfs.js
+++ src/utils/departuresFromGtfs.js
@@ -6,7 +6,6 @@ const ScheduleRelationship = GtfsRealtimeBindings.transit_realtime.TripUpdate.St
 export default function departuresFromGtfs (gtfsSchedule, gtfsTripUpdates, stopIds) {
   if (gtfsSchedule?.routes === undefined ||
       gtfsSchedule?.stops === undefined ||
-      gtfsSchedule?.stopTimes === undefined ||
       gtfsSchedule?.trips === undefined ||
       gtfsTripUpdates === undefined) {
     return undefined
@@ -15,14 +14,9 @@ export default function departuresFromGtfs (gtfsSchedule, gtfsTripUpdates, stopI
   const routesById = buildIndex(gtfsSchedule.routes, (route) => route.routeId)
   const stopsById = buildIndex(gtfsSchedule.stops, (stop) => stop.stopId)
   const tripsById = buildIndex(gtfsSchedule.trips, (trip) => trip.tripId)
-  const stopTimesByTripId = gtfsSchedule.stopTimes.reduce((map, current) => {
-    map[current.tripId] ??= []
-    map[current.tripId].push(current)
-    return map
-  })
 
   const stops = [...new Set(stopIds)].map((stopId) => stopsById[stopId]).filter(Boolean)
-  const updates = earliestStopTimeUpdates(gtfsTripUpdates.entity.map((entity) => entity.tripUpdate), tripsById, stopTimesByTripId)
+  const updates = earliestStopTimeUpdates(gtfsTripUpdates.entity.map((entity) => entity.tripUpdate), tripsById)
 
   const departures = []
   stops.forEach((stop) => {
@@ -53,7 +47,7 @@ function buildIndex (collection, computeKey) {
   return index
 }
 
-function earliestStopTimeUpdates (tripUpdates, tripsById, stopTimesByTripId) {
+function earliestStopTimeUpdates (tripUpdates, tripsById) {
   const stopTimeUpdates = {}
   tripUpdates.forEach((tripUpdate) => {
     const trip = tripsById[tripUpdate.trip.tripId]
@@ -69,7 +63,6 @@ function earliestStopTimeUpdates (tripUpdates, tripsById, stopTimesByTripId) {
       const time = fromUnixTime((stopTimeUpdate.departure || stopTimeUpdate.arrival).time)
 
       if (isPast(time)) return
-      if (isLastStopInTrip(tripId, stopId, stopTimesByTripId)) return
 
       stopTimeUpdates[stopId] ??= {}
       stopTimeUpdates[stopId][routeId] ??= {}
@@ -88,11 +81,4 @@ function earliestStopTimeUpdates (tripUpdates, tripsById, stopTimesByTripId) {
     })
   })
   return departures
-}
-
-function isLastStopInTrip (tripId, stopId, stopTimesByTripId) {
-  const lastStopId = stopTimesByTripId[tripId].reduce((target, current) => {
-    return Number(target.stopSequence) > Number(current.stopSequence) ? target : current
-  }).stopId
-  return lastStopId === stopId
 }
