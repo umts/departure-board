6d4024050f7f286d7ae0bd60160ce7fc2e3627a8
benmelz <ben@melz.me>
2026-03-10 11:17:41 -0400

    update config

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import departuresFromGtfs from './utils/departuresFromGtfs.js'
 import alertsFromGtfs from './utils/alertsFromGtfs.js'
 
 export default function App () {
-  const { gtfsScheduleUrl, gtfsRealtimeTripUpdatesUrl, stopIds, routeIds } = useConfig()
+  const { gtfsScheduleUrl, gtfsRealtimeTripUpdatesUrl, gtfsRealtimeAlertsUrl, stopIds, routeIds } = useConfig()
 
   const scheduleResolver = useFetchResolver(gtfsScheduleUrl)
   const gtfsSchedule = useGtfsSchedule(scheduleResolver, 24 * 60 * 60 * 1000)
@@ -13,16 +13,11 @@ export default function App () {
   const tripUpdatesResolver = useFetchResolver(gtfsRealtimeTripUpdatesUrl)
   const gtfsTripUpdates = useGtfsRealtime(tripUpdatesResolver, 30 * 1000)
 
-  const alertsResolver = useFetchResolver('http://localhost:9292/gtfs-rt/alerts')
+  const alertsResolver = useFetchResolver(gtfsRealtimeAlertsUrl)
   const gtfsAlerts = useGtfsRealtime(alertsResolver, 30 * 1000)
 
   const stops = departuresFromGtfs(gtfsSchedule, gtfsTripUpdates, stopIds, routeIds)
-  // const alerts = alertsFromGtfs(gtfsAlerts, stopIds, routeIds)
-  const alerts = [
-    { header: 'Test1', description: 'Test1 Alert', routes: [{ id: 1, name: '30' }], effect: 'Detour' },
-    { header: 'Test2', description: 'Test2 Alert',
-      routes: [{ id: 2, name: '34' }, { id: 3, name: '35' }], effect: 'Stop closed' },
-  ]
+  const alerts = alertsFromGtfs(gtfsAlerts, stopIds, routeIds)
 
   return (
     <>
--- src/hooks/useConfig.js
+++ src/hooks/useConfig.js
@@ -10,6 +10,9 @@ export default function useConfig () {
       gtfsRealtimeTripUpdatesUrl: searchParams.get('gtfsRealtimeTripUpdatesUrl') ||
         import.meta.env.VITE_GTFS_REALTIME_TRIP_UPDATES_URL ||
         DEFAULT_GTFS_REALTIME_TRIP_UPDATES_URL,
+      gtfsRealtimeAlertsUrl: searchParams.get('gtfsRealtimeAlertsUrl') ||
+        import.meta.env.VITE_GTFS_REALTIME_ALERTS_URL ||
+        DEFAULT_GTFS_REALTIME_ALERTS_URL,
       stopIds: parseArray(searchParams.get('stopIds') || import.meta.env.VITE_STOP_IDS),
       routeIds: parseArray(searchParams.get('routeIds') || import.meta.env.VITE_ROUTE_IDS),
     }
@@ -18,6 +21,7 @@ export default function useConfig () {
 
 const DEFAULT_GTFS_SCHEDULE_URL = 'https://gtfs-cache.admin.umass.edu/gtfs'
 const DEFAULT_GTFS_REALTIME_TRIP_UPDATES_URL = 'https://gtfs-cache.admin.umass.edu/gtfs-rt/trip-updates'
+const DEFAULT_GTFS_REALTIME_ALERTS_URL = 'https://gtfs-cache.admin.umass.edu/gtfs-rt/alerts'
 
 function parseArray (arg) {
   return arg?.split(',')?.filter((item) => !!(item))
