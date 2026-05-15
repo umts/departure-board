66939a581c926ac1115111924b6cd16386663cda
benmelz <ben@melz.me>
2026-05-15 13:38:26 -0400

    language softening
    Co-authored-by: Matt Moretti <werebus@gmail.com>

--- src/components/MigrateWarning.jsx
+++ src/components/MigrateWarning.jsx
@@ -6,7 +6,7 @@ export default function MigrateWarning () {
       <div><i className='fa-solid fa-triangle-exclamation' /></div>
       <div>
         The previous departure board application has been replaced and this display has been automatically
-        redirected to the new application as a courtesy. The IT Administrator for this display must visit
+        redirected to the new application as a courtesy. The Administrator for this display should visit
         https://github.com/umts/departure-board and follow the reconfiguration instructions to remove this message.
       </div>
     </div>
