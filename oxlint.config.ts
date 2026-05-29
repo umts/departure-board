b3eeb135dd21bcb919cc2b091bc71a8e0abd4b2e
benmelz <ben@melz.me>
2026-05-29 11:59:07 -0400

    some rule configurations

--- oxlint.config.ts
+++ oxlint.config.ts
@@ -20,4 +20,23 @@ export default defineConfig({
     perf: "error",
     restriction: "error",
   },
+  rules: {
+    "no-undefined": "off",
+    "import/no-default-export": "off",
+    "import/no-relative-parent-imports": "off",
+    "import/no-unassigned-import": "off",
+    "oxc/no-async-await": "off",
+    "oxc/no-rest-spread-properties": "off",
+    "react/react-in-jsx-scope": "off",
+  },
+  overrides: [
+    {
+      files: ["*.test.{js,jsx}"],
+      rules: {
+        "max-lines": "off",
+        "max-lines-per-function": "off",
+        "vitest/require-test-timeout": "off",
+      },
+    },
+  ],
 });
