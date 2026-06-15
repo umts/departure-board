bf97c1ea1aaf05bd8563e1063ffa2b3ab95f4c08
Aryanvakil <aryanvkl82@gmail.com>
2026-06-15 15:55:05 -0400

    Implement vertical auto scroll behavior

--- src/components/DepartureBoard.jsx
+++ src/components/DepartureBoard.jsx
@@ -1,4 +1,4 @@
-import { useMemo } from "react";
+import { useMemo, useEffect, useRef } from "react";
 import AlertCarousel from "./AlertCarousel.jsx";
 import classNames from "./DepartureBoard.module.css";
 import MigrateWarning from "./MigrateWarning.jsx";
@@ -11,8 +11,49 @@ export default function DepartureBoard({ migrateWarning, stops, alerts }) {
     () => ({ fontSize: `min(${3 / width}vw, ${3 / height}vh)` }),
     [width, height],
   );
+
+  const containerRef = useRef(null);
+  const scrollRef = useRef(0);
+  const rafRef = useRef(null);
+  const runningRaf = useRef(true);
+
+  useEffect(() => {
+    const element = containerRef.current;
+    if (!element) return;
+
+    scrollRef.current = element.scrollTop || 0;
+
+    let lastTime = performance.now();
+
+    const speed = 15;
+
+    const animate = (now) => {
+      if (!runningRaf.current) return;
+    
+      if (!lastTime) lastTime = now;
+    
+      const delta = (now - lastTime) / 1000;
+      lastTime = now;
+    
+      scrollRef.current += speed * delta;
+    
+      const maxScroll = element.scrollHeight - element.clientHeight;
+    
+      if (scrollRef.current >= maxScroll) {
+        scrollRef.current = 0;
+      }
+    
+      element.scrollTop = scrollRef.current;
+    
+      rafRef.current = requestAnimationFrame(animate);
+    };
+    rafRef.current = requestAnimationFrame(animate);
+
+    return () => cancelAnimationFrame(rafRef.current);
+  }, []);
+
   return (
-    <div className={classNames["departure-board"]} style={style}>
+    <div ref={containerRef} className={classNames["departure-board"]} style={style}>
       {migrateWarning && <MigrateWarning />}
       <StopGrid stops={stops} width={width} />
       <AlertCarousel alerts={alerts} />
