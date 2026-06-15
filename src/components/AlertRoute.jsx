2fed10f1731ffb9bdfc51a1cc0a850bd4ad754b5
Aryanvakil <aryanvkl82@gmail.com>
2026-06-15 12:01:41 -0400

    Specify padding for the alert route background

--- src/components/AlertRoute.jsx
+++ src/components/AlertRoute.jsx
@@ -2,7 +2,7 @@ import { useMemo } from "react";
 
 export default function AlertRoute({ name, color, routeTextColor }) {
   const style = useMemo(
-    () => ({ "background-color": `#${color}`, color: `#${routeTextColor}` }),
+    () => ({ "background-color": `#${color}`, color: `#${routeTextColor}`, padding: "0 0.75rem" }),
     [color, routeTextColor],
   );
   return <span style={style}> {name} </span>;
