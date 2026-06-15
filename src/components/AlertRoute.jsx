05e000975c4ffea2539a751ce16c1a532e8346b8
Aryanvakil <aryanvkl82@gmail.com>
2026-06-15 11:46:30 -0400

    lint and format

--- src/components/AlertRoute.jsx
+++ src/components/AlertRoute.jsx
@@ -2,8 +2,8 @@ import { useMemo } from "react";
 
 export default function AlertRoute({ name, color, routeTextColor }) {
   const style = useMemo(
-    () => ({ 'background-color': `#${color}`, color: `#${routeTextColor}` }),
-    [color],
+    () => ({ "background-color": `#${color}`, color: `#${routeTextColor}` }),
+    [color, routeTextColor],
   );
   return <span style={style}> {name} </span>;
 }
